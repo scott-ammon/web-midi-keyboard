9079318a85d2c60416f478c8557c79c11f21302b
Scott Ammon <scott.ammon@slalom.com>
2020-05-08 12:08:30 -0700

    corrected key mappings

--- src/keyMap.js
+++ src/keyMap.js
@@ -1,121 +1,121 @@
 export const keyMap = {
   48: {
-    "note": "c2",
+    "note": "c3",
     "color": "white",
     "offset": 0
   },
   49: {
-    "note": "cs2",
+    "note": "cs3",
     "color": "black",
     "offset": 26
   },
   50: {
-    "note": "d2",
+    "note": "d3",
     "color": "white",
     "offset": 40
   },
   51: {
-    "note": "ds2",
+    "note": "ds3",
     "color": "black",
     "offset": 73
   },
   52: {
-    "note": "e2",
+    "note": "e3",
     "color": "white",
     "offset": 80
   },
   53: {
-    "note": "f2",
+    "note": "f3",
     "color": "white",
     "offset": 120
   },
   54: {
-    "note": "fs2",
+    "note": "fs3",
     "color": "black",
     "offset": 144
   },
   55: {
-    "note": "g2",
+    "note": "g3",
     "color": "white",
     "offset": 160
   },
   56: {
-    "note": "gs2",
+    "note": "gs3",
     "color": "black",
     "offset": 188
   },
   57: {
-    "note": "a2",
+    "note": "a3",
     "color": "white",
     "offset": 200
   },
   58: {
-    "note": "as2",
+    "note": "as3",
     "color": "black",
     "offset": 232
   },
   59: {
-    "note": "b2",
+    "note": "b3",
     "color": "white",
     "offset": 240
   },
   60: {
-    "note": "c3",
+    "note": "c4",
     "color": "white",
     "offset": 280
   },
   61: {
-    "note": "cs3",
+    "note": "cs4",
     "color": "black",
     "offset": 302
   },
   62: {
-    "note": "d3",
+    "note": "d4",
     "color": "white",
     "offset": 320
   },
   63: {
-    "note": "ds3",
+    "note": "ds4",
     "color": "black",
     "offset": 351
   },
   64: {
-    "note": "e3",
+    "note": "e4",
     "color": "white",
     "offset": 360
   },
   65: {
-    "note": "f3",
+    "note": "f4",
     "color": "white",
     "offset": 400
   },
   66: {
-    "note": "fs3",
+    "note": "fs4",
     "color": "black",
     "offset": 420
   },
   67: {
-    "note": "g3",
+    "note": "g4",
     "color": "white",
     "offset": 440
   },
   68: {
-    "note": "gs3",
+    "note": "gs4",
     "color": "black",
     "offset": 464
   },
   69: {
-    "note": "a3",
+    "note": "a4",
     "color": "white",
     "offset": 480
   },
   70: {
-    "note": "as3",
+    "note": "as4",
     "color": "black",
     "offset": 509
   },
   71: {
-    "note": "b3",
+    "note": "b4",
     "color": "white",
     "offset": 520
   },
