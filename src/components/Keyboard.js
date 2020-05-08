9b53803c3ca2ac6308dcfcec4106048794a9706a
Scott Ammon <scott.ammon@slalom.com>
2020-05-08 09:54:49 -0700

    continuing styling of keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -38,6 +38,16 @@ const Keyboard = () => {
           <div className="white-key"></div>
           <div className="white-key"></div>
           <div className="white-key"></div>
+          <div className="black-key cs2"></div>
+          <div className="black-key ds2"></div>
+          <div className="black-key fs2"></div>
+          <div className="black-key gs2"></div>
+          <div className="black-key as2"></div>
+          <div className="black-key cs3"></div>
+          <div className="black-key ds3"></div>
+          <div className="black-key fs3"></div>
+          <div className="black-key gs3"></div>
+          <div className="black-key as3"></div>
           {pressedKeys}
         </div>
       </div>
