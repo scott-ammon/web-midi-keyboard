66d3498b778c5f3641a54e1a9f3c60ed24ab6471
Scott Ammon <scott.ammon@slalom.com>
2020-06-28 11:07:33 -0700

    tests need to move to npm package

--- src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders page title', () => {
-  const { getByText } = render(<App />);
-  const pageTitle = getByText(/Web MIDI Api Visualizer/i);
-  expect(pageTitle).toBeInTheDocument();
-});
